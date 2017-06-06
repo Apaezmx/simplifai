08a21d9cdbc27228b4e160c2c7b3560dc30ec125
Andres Paez <andres@minatini.com>
2017-06-06 18:00:04 +0000

    Add a start bash file to bring up servers.

---
+++ start.sh
@@ -0,0 +1,15 @@
+sudo apt-get --assume-yes install python-pip python-dev build-essential git libev-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
+
+sudo pip install virtualenv virtualenvwrapper
+
+git clone https://Apaezmx@bitbucket.org/Apaezmx/pusher.git
+
+cd pusher
+
+virtualenv .
+
+source bin/activate
+
+pip install --upgrade pip
+
+pip install -r reqs.txt
